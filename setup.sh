769727afe60a4ce319ff991891a799bc0247a5b0
MrPekar98 <jm14ok@cs.aau.dk@a256-gc1-19.srv.aau.dk>
2023-10-12 11:38:53 +0200

    Setup script

--- setup.sh
+++ setup.sh
@@ -7,4 +7,6 @@ gzip -d cc.en.300.bin.gz
 mkdir -p embeddings/
 mv cc.en.300.bin embeddings/
 
-python3 generate_aliases.py
+wget https://downloads.dbpedia.org/repo/dbpedia/wikidata/alias/2021.02.01/alias.ttl.bz2
+bzip2 -dk alias.ttl.bz2
+rm alias.ttl.bz2
